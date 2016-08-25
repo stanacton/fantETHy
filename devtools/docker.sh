6cc4cb568d6fc4e191a75b341db9bb675c0b4ac3
stanacton <stanacton@gmail.com>
2016-08-25 17:36:55 +0100

    upgraded to 16.04

--- devtools/docker.sh
+++ devtools/docker.sh
@@ -6,8 +6,10 @@ apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 rm -rf /etc/apt/sources.list.d/docker.list
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
-
+# 14.04 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
+# 16.04 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >> /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
+              
 apt-get update 
 
 apt-get purge lxc-docker

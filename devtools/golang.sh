79f0299ce705db0828b06e8ed61942ec26741d13
stanacton <stanacton@gmail.com>
2016-08-26 23:57:32 +0100

    fixed path variables

--- devtools/golang.sh
+++ devtools/golang.sh
@@ -1,4 +1,4 @@
- #!/usr/bin/env bash
+ #!/bin/bash
 export GOPATH=/home/vagrant/src/GOPATH
 
 add-apt-repository ppa:ubuntu-lxc/lxd-stable
@@ -6,15 +6,15 @@ apt-get update
 apt-get install -y golang
 
 if [ ! -d "$DIRECTORY" ]; then
-    mkdir -p ~/src/GOPATH
+    mkdir -p /home/vagrant/src/GOPATH
 fi
 
-if grep -q GOPATH ~/.bashrc; then
+if grep -q GOPATH /etc/environment; then
    echo "GOPATH already exists"
 else 
    echo "adding GOPATH to .bashrc"
-   echo "export GOPATH=~/src/GOPATH" >> ~/.bashrc
-   echo "export PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
+   sudo echo "GOPATH=/home/vagrant/src/GOPATH" >> /etc/environment
+   sudo echo "PATH=$PATH:$GOPATH/bin" >> /etc/environment
 fi
 
 go install tbd
